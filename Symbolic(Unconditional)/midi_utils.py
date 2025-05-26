0c71e45ee7ae1ddf40a8d86645c423d8341921e6
Terence Tan <tetan1021@gmail.com>
2025-05-25 23:04:21 -0700

    Lo-Fi Model

--- Symbolic(Unconditional)/midi_utils.py
+++ Symbolic(Unconditional)/midi_utils.py
@@ -16,25 +16,17 @@ def midi_to_note_duration_sequence(filename):
             notes.append((msg.note, int(duration * 480)))  # scale duration for MIDI ticks
             del note_on_times[msg.note]
     return notes
-
-def note_sequence_to_midi(note_sequence, filename, velocity=64, tempo=500000, note_length=120):
+#change the tempo for a slower BPM
+#increase note_length for longer notes, more lofi feel
+def note_sequence_to_midi(note_sequence, filename, velocity=64, tempo=750000, note_length=240, program=4):
     import mido
     mid = mido.MidiFile()
     track = mido.MidiTrack()
     mid.tracks.append(track)
     track.append(mido.MetaMessage('set_tempo', tempo=tempo))
+    track.append(mido.Message('program_change', program=program, time=0))
+    
     for note in note_sequence:
         track.append(mido.Message('note_on', note=note, velocity=velocity, time=0))
         track.append(mido.Message('note_off', note=note, velocity=velocity, time=note_length))
-    mid.save(filename)
-
-def note_duration_sequence_to_midi(note_duration_sequence, filename, velocity=64, tempo=500000):
-
-    mid = mido.MidiFile()
-    track = mido.MidiTrack()
-    mid.tracks.append(track)
-    track.append(mido.MetaMessage('set_tempo', tempo=tempo))
-    for note, duration in note_duration_sequence:
-        track.append(mido.Message('note_on', note=note, velocity=velocity, time=0))
-        track.append(mido.Message('note_off', note=note, velocity=velocity, time=duration))
     mid.save(filename)
---
+++ Symbolic(Unconditional)/writeup(Unconditional).md
@@ -0,0 +1,15 @@
+Symbolic Unconditional Music Generation
+=====
+
+# Model: Long Short Term Memory (LSTM)
+
+What is a LSTM? It is a type of Recurrent Neural Network (RNN) that is able to learn about Long Short Term dependencies from sequences
+
+DataSets
+- CSE 153R SP25, Midi file data from Assignment 1, Task 1. Located in the Assignment1(Task1_mdidis) folder.
+- From Kaggle(Lofi-Hip-Hop), you can find the midi files in the Lo-Fi folder or you can read/download it from: https://www.kaggle.com/datasets/zakarii/lofi-hip-hop-midi?resource=download (Last accessed May 25, 2025).
+- In the data folder, is a bunch of popular pop songs I personally enjoy. And was able to gather up some of it in midi form.
+
+
+  
+
